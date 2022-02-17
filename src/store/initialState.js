3ec3d9f73af87093279ab70bcde691669e8ba688
FernandoLC <fernando.lucena@gnp.com.mx>
2022-02-16 21:41:49 -0600

    config router, update font, example redux

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -2,10 +2,10 @@ export const initialState = {
   level1: {
     level2: {
       level3: {
-        propertyLevel3: 'Template Webpack 5',
+        propertyLevel3: 'propertyLevel3',
       },
-      propertyLevel2: 'property level 2',
+      propertyLevel2: 'propertyLevel2',
     },
-    propertyLevel1: 'property level 1',
+    propertyLevel1: 'propertyLevel1',
   },
 };
